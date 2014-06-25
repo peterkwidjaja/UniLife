46be851ff7a55e25d6dac584a98d09bf86f2cf0f
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2014-06-25 17:50:25 +0800

    prepare module list

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
 require 'sinatra'
 require 'rubygems'
+require 'json'
+
+class modules
+	def initialize(code, title, desc, credit, workload, preclusion, prereq)
+		@code = code
+		@title = title
+		@description = desc
+		@credit = credit
+		@workload = workload
+		@preclusion = preclusion
+		@prereq = prereq
+
+end
 
 get '/' do
 	@stylepage="css/home-style.css"
 	erb :home
+end
 
+get '/modules' do
+	json = File.read('public/moduleList.json')
+	result = JSON.parse(json)
+	result.each do |key, value|
+		puts key
+	end
 end
