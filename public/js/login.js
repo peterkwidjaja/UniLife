0b616c53058cd6157b2d5596d4d0570d8e769c9c
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2014-08-04 16:26:41 +0800

    clean css sheets for plan page. Add glyphicon for signin and signout

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 function logoutModal(){
 	emptyModal();
 	$('#login-title').append("<center>Sign Out</center>");
-	$('#login-body').append('<center><a href="javascript:logout()" role="button" id="logout-button" class="btn btn-primary">Sign out</a></center>');
+	$('#login-body').append('<center><a href="javascript:logout()" role="button" id="logout-button" class="btn btn-primary">Sign out<span class="glyphicon glyphicon-log-out" style="margin-left: 10px;"></span></a></center>');
 	$('#login-dialog').modal('show');
 }
 function logout(){
@@ -12,7 +12,7 @@ function logout(){
 function login(){
 	emptyModal();
 	$('#login-title').append("<center>Sign in</center>");
-	$('#login-body').append('<center><a href="/auth/nus" role="button" id="login-button" class="btn btn-primary">Sign in with NUSNET ID</a></center>');
+	$('#login-body').append('<center><a href="/auth/nus" role="button" id="login-button" class="btn btn-primary">Sign in with NUSNET ID<span class="glyphicon glyphicon-log-in" style="margin-left: 10px;"></span></a></center>');
 	$('#login-dialog').modal('show');
 }
 function emptyModal(){
