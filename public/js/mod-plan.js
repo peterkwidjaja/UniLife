52267c243e617de55f31ae9e58cb27f0214559ca
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2014-08-04 15:30:27 +0800

    minor changes in plan page and footer

--- public/js/mod-plan.js
+++ public/js/mod-plan.js
@@ -24,7 +24,7 @@ for (i=0; i<8; i++){
 function addModules(sem, module){
 	$.get("/mod/"+ module, function(data){
 		choice[sem-1].addMod(data);
-		$('#content-sem'+sem).append('<tr><td>'+data["ModuleCode"]+'</td><td>'+data["ModuleTitle"]+'</td><td><a href="#" title="remove" class="remove-button"><span class="glyphicon glyphicon-remove-sign" style="color: #000000;"></span></a></td></tr>');
+		$('#content-sem'+sem).append('<tr><td>'+data["ModuleCode"]+'</td><td>'+data["ModuleTitle"]+'</td><td><center><a href="#" title="remove" class="remove-button"><span class="glyphicon glyphicon-remove-sign" style="color: #000000;"></span></a></center></td></tr>');
 		//$('#total-credit'+(sem.toString())).empty();
 		$('#total-credit'+(sem.toString())).html(choice[sem-1].totalMC.toString());
 	},"json");	
