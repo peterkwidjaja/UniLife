1d8e0f4e25b8b74a24c59266e83f50d5f32b0f10
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2014-08-06 12:45:10 +0800

    fix login modal positioning. Add module details at plan page

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 function logoutModal(){
 	emptyModal();
-	$('#login-title').append("<center>Sign Out</center>");
-	$('#login-body').append('<center><a href="javascript:logout()" role="button" id="logout-button" class="btn btn-primary">Sign out<span class="glyphicon glyphicon-log-out" style="margin-left: 10px;"></span></a></center>');
-	$('#login-dialog').modal('show');
+	$('#modalTitle').append("<center>Sign Out</center>");
+	$('#modalBody').append('<center><a href="javascript:logout()" role="button" id="logout-button" class="btn btn-primary">Sign out<span class="glyphicon glyphicon-log-out" style="margin-left: 10px;"></span></a></center>');
+	$('#mainModal').modal('show');
 }
 function logout(){
 	$.post('/logout', function(){
@@ -11,11 +11,11 @@ function logout(){
 }
 function login(){
 	emptyModal();
-	$('#login-title').append("<center>Sign in</center>");
-	$('#login-body').append('<center><a href="/auth/nus" role="button" id="login-button" class="btn btn-primary">Sign in with NUSNET ID<span class="glyphicon glyphicon-log-in" style="margin-left: 10px;"></span></a></center>');
-	$('#login-dialog').modal('show');
+	$('#modalTitle').append("<center>Sign in</center>");
+	$('#modalBody').append('<p><center><a href="/auth/nus" role="button" id="login-button" class="btn btn-primary">Sign in with NUSNET ID<span class="glyphicon glyphicon-log-in" style="margin-left: 10px;"></span></a></center></p>');
+	$('#mainModal').modal('show');
 }
 function emptyModal(){
-	$('#login-title').empty();
-	$('#login-body').empty();
+	$('#modalTitle').empty();
+	$('#modalBody').empty();
 }
--- public/js/mod-plan.js
+++ public/js/mod-plan.js
@@ -24,7 +24,7 @@ for (i=0; i<8; i++){
 function addModules(sem, module){
 	$.get("/mod/"+ module, function(data){
 		choice[sem-1].addMod(data);
-		$('#content-sem'+sem).append('<tr><td>'+data["ModuleCode"]+'</td><td>'+data["ModuleTitle"]+'</td><td><center><a href="#" title="remove" class="remove-button"><span class="glyphicon glyphicon-remove-sign" style="color: #000000;"></span></a></center></td></tr>');
+		$('#content-sem'+sem).append('<tr><td><a href="javascript:showDetails('+"'"+data["ModuleCode"]+"'"+')">'+data["ModuleCode"]+'</a></td><td>'+data["ModuleTitle"]+'</td><td><center><a href="#" title="remove" class="remove-button"><span class="glyphicon glyphicon-remove-sign" style="color: #000000;"></span></a></center></td></tr>');
 		//$('#total-credit'+(sem.toString())).empty();
 		$('#total-credit'+(sem.toString())).html(choice[sem-1].totalMC.toString());
 	},"json");	
@@ -48,4 +48,42 @@ function save(){
 	}
 	var data = sem.join(",");
 	$.post('/plan',{mods: data});
+}
+function showDetails(moduleCode) {
+	$.get("/mod/"+ moduleCode, function(data){
+		emptyDetails();
+		$('#moduleTitle').append(moduleCode+" - "+data["ModuleTitle"]);
+		$('#moduleDescription').append("<p>"+data["ModuleDescription"]+"</p>");
+		$('#moduleCredit').append("<p>Module Credit:  "+data["ModuleCredit"]+"</p>");
+		$('#moduleWorkload').append("<p>Workload:  "+data["Workload"]+"</p>");
+		if(data["Prerequisite"])
+			$('#modulePrereq').append("<p>Prerequisite:  "+data["Prerequisite"]+"</p>");
+		else
+			$('#modulePrereq').append("<p>Prerequisite:  Nil</p>");
+		if(data["Preclusion"])
+			$('#modulePreclus').append("<p>Preclusion:  "+data["Preclusion"]+"</p>");
+		else
+			$('#modulePreclus').append("<p>Preclusion:  Nil</p>");
+		var html="<p>Semester:  ";
+		for(i=0;i<data["History"].length;i++){
+			var temp = data["History"][i]["Semester"];
+			var sem = temp.toString();
+			if(temp==3) sem="Special Term 1";
+			else if(temp==4) sem="Special Term 2";
+			html += sem.toString();
+			if(i!=data["History"].length-1) html += ", ";
+		}
+		html+="</p>";
+		$('#moduleSemester').append(html);
+		$('#moduleDetails').modal('show');
+	},"json");
+}
+function emptyDetails(){
+	$('#moduleTitle').empty();
+	$('#moduleDescription').empty();
+	$('#moduleCredit').empty();
+	$('#moduleWorkload').empty();
+	$('#modulePrereq').empty();
+	$('#modulePreclus').empty();
+	$('#moduleSemester').empty();
 }
